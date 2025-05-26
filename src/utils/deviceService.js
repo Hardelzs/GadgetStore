ebd5ac54b7f1f4588f21773d0fd307c7fd4ca604
Hardelz <135560555+Hardelzs@users.noreply.github.com>
2025-05-26 01:43:16 +0100

    l

--- src/utils/deviceService.js
+++ src/utils/deviceService.js
@@ -1,4 +1,4 @@
-import { storage } from "../lib/Firebase.js";
+import { db, storage } from "../lib/Firebase.js";
 import { collection, addDoc } from "firebase/firestore";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
@@ -25,6 +25,6 @@ export const saveDevice = async (device) => {
   };
 
 
-  const docRef = await addDoc(collection( "devices"), deviceData);
+  const docRef = await addDoc(collection(db, "devices"), deviceData);
   return docRef.id;
 };
