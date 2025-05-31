e721666f293049c8de8eefa04c8cff5fced74273
Hardelz <135560555+Hardelzs@users.noreply.github.com>
2025-05-31 19:15:58 +0100

    fix: correct import path for WaterDrop component in Login.jsx

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,6 +1,7 @@
 import {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
-import WaterDrop from '../components/WaterDrop';
+// import WaterDrop from '../components/WaterDrop';
+import WaterDrop from '../Components/WaterDrop';
 import { DivOrigami } from '../components/DivOrigami';
 
 
