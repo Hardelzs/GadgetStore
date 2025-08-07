e2d0a29ff252709c3af1ff95ecf0dd9d77b3cc84
Hardelz <135560555+Hardelzs@users.noreply.github.com>
2025-08-08 00:14:49 +0100

    fix: update Sidebar and Topbar components for improved mobile responsiveness and styling

--- src/Components/Topbar.jsx
+++ src/Components/Topbar.jsx
@@ -10,7 +10,7 @@ export default function Topbar({ pageName = "Dashboard" }) {
   const [hasNewNotification, setHasNewNotification] = useState(true);
 
   return (
-    <header className="px-4 md:px-6 py-4 fixed top-0 left-0 md:left-52 right-0 z-10 backdrop-blur-md bg-white/70">
+    <header className="px-4 md:px-6 py-4 fixed top-0 left-0 md:left-52 right-0 z-10 backdrop-blur-md bg-white/70 hidden md:block">
       <div className="flex flex-col md:flex-row justify-between items-center gap-4">
         {/* Left Section */}
         <div className="text-[#929DAE] w-full md:w-auto">
@@ -20,12 +20,12 @@ export default function Topbar({ pageName = "Dashboard" }) {
             </NavLink>
             <span>/ {pageName}</span>
           </h1>
-          <h2 className="font-bold text-black text-lg md:text-xl">{pageName}</h2>
+          <h2 className="font-bold text-green-300 text-lg md:text-xl">{pageName}</h2>
         </div>
 
         {/* Right Section */}
         <div className="flex items-center gap-3 w-full md:w-auto">
-          <div className="relative px-4 py-2">
+          <div className="relative px-4 py-2 ">
             <button onClick={() => setShowSidebar(true)} className="relative">
               <IoMdNotificationsOutline className="text-xl cursor-pointer hover:text-blue-500" />
               {hasNewNotification && (
